e816fd972acd4640857f4bb7e904545f29eb5387
Prathik Pai <prathik@logichive.in>
2021-06-24 19:49:12 +0530

    Fixes

--- cvat-canvas3d/src/typescript/canvas3dView.ts
+++ cvat-canvas3d/src/typescript/canvas3dView.ts
@@ -240,7 +240,7 @@ export class Canvas3dViewImpl implements Canvas3dView, Listener {
                                 points,
                                 label,
                             },
-                            continue: undefined,
+                            continue: true,
                             duration: 0,
                         },
                     }),
@@ -449,6 +449,12 @@ export class Canvas3dViewImpl implements Canvas3dView, Listener {
                     viewType.controls.mouseButtons.right = CameraControls.ACTION.NONE;
                 } else {
                     viewType.controls = new CameraControls(viewType.camera, viewType.renderer.domElement);
+                    viewType.controls.mouseButtons.left = CameraControls.ACTION.NONE;
+                    viewType.controls.mouseButtons.right = CameraControls.ACTION.NONE;
+                    viewType.controls.mouseButtons.wheel = CameraControls.ACTION.NONE;
+                    viewType.controls.touches.one = CameraControls.ACTION.NONE;
+                    viewType.controls.touches.two = CameraControls.ACTION.NONE;
+                    viewType.controls.touches.three =CameraControls.ACTION.NONE;
                 }
                 viewType.controls.minDistance = CONST.MIN_DISTANCE;
                 viewType.controls.maxDistance = CONST.MAX_DISTANCE;
@@ -525,6 +531,7 @@ export class Canvas3dViewImpl implements Canvas3dView, Listener {
 
     private startAction(view: any, event: MouseEvent): void {
         if (event.detail !== 1) return;
+        if (this.model.mode === Mode.DRAG_CANVAS) return;
         const { clientID } = this.model.data.activeElement;
         if (clientID === 'null') return;
         const canvas = this.views[view as keyof Views].renderer.domElement;
@@ -549,6 +556,7 @@ export class Canvas3dViewImpl implements Canvas3dView, Listener {
 
     private moveAction(view: any, event: MouseEvent): void {
         event.preventDefault();
+        if (this.model.mode === Mode.DRAG_CANVAS) return;
         const { clientID } = this.model.data.activeElement;
         if (clientID === 'null') return;
         const canvas = this.views[view as keyof Views].renderer.domElement;
@@ -609,6 +617,7 @@ export class Canvas3dViewImpl implements Canvas3dView, Listener {
 
     private completeActions(): void {
         const { scan, detected } = this.action;
+        if (this.model.mode === Mode.DRAG_CANVAS) return;
         if (!detected) {
             this.resetActions();
             return;
@@ -806,12 +815,21 @@ export class Canvas3dViewImpl implements Canvas3dView, Listener {
             this.setupObjects();
         } else if (reason === UpdateReasons.DRAG_CANVAS) {
             this.dispatchEvent(
-                new CustomEvent(this.mode === Mode.DRAG_CANVAS ? 'canvas.dragstart' : 'canvas.dragstop', {
+                new CustomEvent(this.model.mode === Mode.DRAG_CANVAS ? 'canvas.dragstart' : 'canvas.dragstop', {
                     bubbles: false,
                     cancelable: true,
                 }),
             );
             this.model.data.activeElement.clientID = 'null';
+            if(this.model.mode === Mode.DRAG_CANVAS){
+                const {controls} = this.views.perspective;
+                controls.mouseButtons.left = CameraControls.ACTION.ROTATE;
+                controls.mouseButtons.right = CameraControls.ACTION.DOLLY;
+                controls.mouseButtons.wheel = CameraControls.ACTION.TRUCK;
+                controls.touches.one = CameraControls.ACTION.TOUCH_ROTATE;
+                controls.touches.two = CameraControls.ACTION.TOUCH_DOLLY_TRUCK;
+                controls.touches.three =CameraControls.ACTION.TOUCH_TRUCK;
+            }
             this.setupObjects();
         } else if (reason === UpdateReasons.CANCEL) {
             if (this.mode === Mode.DRAW) {
@@ -823,7 +841,14 @@ export class Canvas3dViewImpl implements Canvas3dView, Listener {
             }
             this.model.data.groupData.grouped = [];
             this.setHelperVisibility(false);
-            this.mode = Mode.IDLE;
+            this.model.mode = Mode.IDLE;
+            const {controls} = this.views.perspective;
+            controls.mouseButtons.left = CameraControls.ACTION.NONE;
+            controls.mouseButtons.right = CameraControls.ACTION.NONE;
+            controls.mouseButtons.wheel = CameraControls.ACTION.NONE;
+            controls.touches.one = CameraControls.ACTION.NONE;
+            controls.touches.two = CameraControls.ACTION.NONE;
+            controls.touches.three =CameraControls.ACTION.NONE;
             this.dispatchEvent(new CustomEvent('canvas.canceled'));
         } else if (reason === UpdateReasons.FITTED_CANVAS) {
             this.dispatchEvent(new CustomEvent('canvas.fit'));
